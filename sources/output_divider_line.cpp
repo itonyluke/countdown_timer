e5911de39ab2fd29488686758d4410169d6d2218
Waves Anisha <itonyluke@gmail.com>
2021-12-09 17:54:53 +0300

    comment out output_divider_line() function

--- sources/output_divider_line.cpp
+++ sources/output_divider_line.cpp
@@ -4,25 +4,29 @@
 
 #include "../include/countdown_timer.hpp"
 
+//outputs the divider line that consists of '*' symbol only for the entire terminal window width
 void output_divider_line()
 {
-	t_w w;
-	int term_fd;
-
-	term_fd = open("/dev/tty", O_RDONLY);
-	if (term_fd == -1)
-	{
-		std::cout << "/dev/tty: fd == -1" << std::endl;
-		exit(EXIT_FAILURE);
-	}
-	/* Get window size of terminal. */
-	if (ioctl(term_fd, TIOCGWINSZ, &w) == -1)
-		std::cout << "ioctl == -1" << std::endl;
-//	std::cout << "\033[90mThis terminal window is\n" << w.ws_rows << " rows by " << w.ws_cols << " columns\033[0m\n";
-
-	for (unsigned int i = 0; i < w.ws_cols; i++)
-		std::cout << DARK_GREY << "*";
-	std::cout << DEFAULT;
-	return ;
+//	t_w w;
+//	int term_fd;
+//
+//	term_fd = open("/dev/tty", O_RDONLY);
+//	if (term_fd == -1)
+//	{
+//		std::cout << "/dev/tty: fd == -1" << std::endl;
+//		exit(EXIT_FAILURE);
+//	}
+//	//Get terminal window size
+//	if (ioctl(term_fd, TIOCGWINSZ, &w) == -1)
+//		std::cout << "ioctl == -1" << std::endl;
+//
+//	//	if you would like to see actual values uncomment the lines below
+//	//	std::cout << DARK_GREY << "This terminal window is\n" << w.ws_rows << " rows by " << w.ws_cols << " columns" << DEFAULT << std::endl;
+//	//	std::cout << DARK_GREY << "(" << w.ws_xpixel << " by "<< w.ws_ypixel << " pixels)" << DEFAULT << std::endl;
+//
+//	for (unsigned int i = 0; i < w.ws_cols; i++)
+//		std::cout << DARK_GREY << "*";
+//	std::cout << DEFAULT;
+//	return ;
 }
 
